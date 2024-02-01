eb01aeb90b6d2e6f31d288a1d86cb3353a06eb47
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-02-01 15:01:36 +0530

    Create Subsets.cpp

---
+++ Subsets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+void solve(vector<int> nums,vector<int> output,int index,vector<vector<int>> &ans){
+    if(index>=nums.size()){
+        ans.push_back(output);
+        return;
+    }
+    solve(nums,output,index+1,ans);
+    output.push_back(nums[index]);
+    solve(nums,output,index+1,ans);
+}
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index = 0;
+        solve(nums,output,index,ans);
+        return ans;
+    }
+};
